a56ee040bfc568bd23a55dddfbb395ed36bbc615
Serg5858 <ss3@ro.ru>
2022-10-06 15:30:50 +0300

    Again

--- src/test/java/ru/netology/webformtest/WebformTestV1.java
+++ src/test/java/ru/netology/webformtest/WebformTestV1.java
@@ -1,20 +1,24 @@
 package ru.netology.webformtest;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static java.awt.SystemColor.text;
 
 public class WebformTestV1 {
 
 @Test
-    void shouldMakeConfirm(){
+    void shouldMakeConfirm() throws InterruptedException {
     Selenide.open("http://localhost:9999");
     $("[data-test-id = 'name'] input").setValue("Иван Иванов");
     $ ("[data-test-id = 'phone'] input").setValue("+79003170909");
     $ ("[data-test-id = 'agreement']").click();
     $$("button").find(Condition.exactText("Продолжить")).click();
+    $("[data-test-id=order-success]").getText();
+    Thread.sleep(5000);
 
 }
 
